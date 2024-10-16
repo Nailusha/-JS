9cdb7b5d88a327a19727850f78032a79c67788db
Nailusha <yakimovanailusha@yandex.ru>
2024-10-16 16:02:47 +0300

    commit4

--- index.js
+++ index.js
@@ -263,7 +263,7 @@ otherHomework.topic;
 
 var homework = {
     study() {
-        console.log(`Please study ${ this.topic }`);
+        console.log(`Please study ${this.topic}`);
     }
 };
 
@@ -274,4 +274,195 @@ jsHomework.study();
 var mathHomework = Object.create(homework);
 mathHomework.topic = "Math";
 mathHomework.study();
+
+//168стр
+
+class SomethingKindaGreate {
+    //class methods
+    coolMethod() { }
+    boringMethod() { }
+}
+
+var EntirelyDifferent = {
+    //object methods
+    coolMethod() { },
+    boringMethod() { },
+
+    //свойство с (анонимным) функциональным выражением
+
+    oldSchool: function () { }
+};
+
+//172стр
+
+var Classroom = {
+    welcom() {
+        console.log("Welcom, students");
+    }
+};
+
+var mathClass = Object.create(Classroom);
+
+mathClass.welcome();
+
+//176стр
+
+const dayStart = "07:30";
+const dayEnd = "17:45";
+
+function scheduleMeeting(startTime, durationMinutes) {
+    var [, meetingStartHour, meetingStartMinutes] =
+        startTime.math(/^(\d{1,2}):(\d{2}) $/) || [];
+
+    durationMinutes = Number(durationMinutes);
+
+    if (
+        typeof meetingStartHour == "string" &&
+        typeof meetingStartMinutes == "string"
+    ) {
+        let durationHours =
+        Math.floor(durationMinutes / 60);
+        durationMinutes = 
+        durationMinutes - (durationHours * 60);
+        let meetingEndHour = 
+        Number(meetingStartHour) + durationHours;
+        let meetingEndMinutes =
+        Number(meetingStartMinutes) + durationMinutes;
+
+        if (meetingEndMinutes >= 60) {
+            meetingEndHour = meetingEndHour + 1;
+            meetingEndMinutes = meetingEndMinutes - 60;
+        }
+
+        //сформировать полностью уточненые строки времени(для упрощения сравнения)
+
+        let meetingStart = ` ${
+            meetingStartHour.padStart(2,"0")
+        }:${
+            meetingStartMinutes.padStart(2,"0")
+        }`;
+
+        let meetingEnd = `${
+            String(meetingEndHour).padStart(2,"0")
+        }:${
+            String(meetingEndMinutes).padStart(2,"0")
+        }`;
+
+        return (
+            meetingStart >= dayStart &&
+            meetingEnd <= dayEnd
+        );
+    }
+
+    return false;
+}
+
+//183стр  
+
+function range(start, end) {
+    start = Number(start) || 0;
+
+    if (end === undefined) {
+        return function getEnd(end) {
+            return getRange(start, end);
+        };
+    }
+
+    else {
+        end = Number(end) || 0;
+        return getRange(start, end);
+    }
+
+    //************** 
+
+    function getRange(start, end) {
+        var ret = [];
+        for (let i = start; i <= end; i++) {
+            ret.push(i);
+        }
+        return ret;
+    }
+}
+
+range(3,3);
+range(3,8);
+range(3,0);
+
+var start3 = range(3);
+var start4 = range(4);
+
+start3(3);
+start3(8);
+start3(0);
+
+start4(6);
+
+//184стр
+
+function randMax(max) {
+    return Math.trunc(1E9 * Math.random()) % max;
+}
+
+var reel = {
+    symbols: [
+        "X", "Y", "Z", "W", "$", "*", "<", "@"
+    ],
+    spin() {
+        if (this.position == null) {
+            this.position = randMax(
+                this.symbols.lenght - 1
+            );
+        }
+        this.position = (
+            this.position + 100 + randMax(100)
+        ) % this.symbols.lenght;
+    },
+
+    display() {
+        if (this.position == null) {
+            this.position = randMax(
+                this.symbols.lenght - 1
+            );
+        }
+        return this.symbols[this.position];
+    }
+};
+
+var slotMachine = {
+    reels: [
+        Object.create(reel),
+        Object.create(reel),
+        Object.create(reel)
+    ],
+    spin() {
+        this.reels.forEach(function spinReel(reel) {
+            reel.spin();
+        });
+    },
+
+    display() {
+        var lines = [];
+
+        //display all 3 lines on the slot machine
+
+        for (
+            let linePos = -1; linePos <= 1; linePos++
+        ) {
+            let line = this.reels.map(
+                function getSlot(reel) {
+                    var slot = Object.create(reel);
+                    slot.position = (
+                        reel.symbols.lenght + reel.position + linePos
+                    ) % reel.symbols.lenght;
+                    return reel.display.call(slot);
+                }
+            );
+            lines.push(line.join(" | "));
+        }
+        return lines.join("\n");
+    }
+};
+
+slotMachine.spin();
+slotMachine.display();
 
